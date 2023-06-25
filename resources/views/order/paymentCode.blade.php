8b1a9674fe5ece3b75173549c6b67c0d59169d52
rickygunawan <rickygunawan266@gmail.com>
2023-06-26 03:18:01 +0700

    update ui payment code

--- resources/views/order/paymentCode.blade.php
+++ resources/views/order/paymentCode.blade.php
@@ -1,38 +1,37 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <p>Thank You For Order</p>
-    <p id="message"></p>
-    <p>Kode Pembayaran anda : <span id="paymentCode"></span></p>
-    <p></p>
-    <div class="card-body">
+@extends('layouts.customer')
 
+@section('content')
+    <div class="container-xxl py-5" id="info">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h5 class="section-title ff-secondary text-center text-primary fw-normal">Order</h5>
+                <p>
+                    <img src="/assets/img/check.gif" alt="check-icon" height="100px"  data-wow-delay="0.1s">
+                </p>
+                <h5 class="text-success fw-bold">Order Berhasil</h5>
+                <h6>Kode pemesanan anda <span id="paymentCode"></span></h6>
+                <p class="mb-5 fst-italic" id="message"></p>
+            </div>
+        </div>
     </div>
+@endsection
 
-
-</body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-
-<script>
-    $(document).ready(function (){
-        const segment = window.location.pathname.split('/')
-        $.ajax({
-            'method': 'GET',
-            'url': '/api/payment/'+segment[4],
-            success: function (data){
-                $('#message').html(data.message);
-                $('#paymentCode').html(data.paymentCode)
-            },
-            error: function (data){
-                window.location.href = '/dinein/registration';
-            }
+@push('script')
+    <script>
+        $(document).ready(function (){
+            const segment = window.location.pathname.split('/')
+            window.location.href = document.URL + '#info'
+            $.ajax({
+                'method': 'GET',
+                'url': '/api/payment/'+segment[4],
+                success: function (data){
+                    $('#message').html(data.message);
+                    $('#paymentCode').html(data.paymentCode)
+                },
+                error: function (data){
+                    window.location.href = '/dinein/registration';
+                }
+            })
         })
-    })
-</script>
-</html>
+    </script>
+@endpush
